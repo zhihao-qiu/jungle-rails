4ac3a84708105ad15d9ba448ca11f41959d5621f
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-06-28 10:54:51 -0700

    Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,6 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: Rails.configuration.basic_auth[:username], password: Rails.configuration.basic_auth[:password]
 
   def show
+    @categories = Category.all.order(id: :asc)
   end
 end
