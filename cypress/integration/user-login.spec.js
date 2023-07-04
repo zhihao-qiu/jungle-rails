8623eb18a5694cfa6d69ff75cac87f3b43e52e95
zhihao-qiu <zhihao.qiu@hotmail.com>
2023-07-04 14:08:30 -0700

    Feature Spec: User Login

---
+++ cypress/integration/user-login.spec.js
@@ -0,0 +1,67 @@
+describe('User Signup & Login', () => {
+  context('signup and login sequence', () => {
+
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
+
+    it("should signup and navigate to homepage", () => {
+      cy.contains('Signup')
+        .click();
+
+      cy.get('input[name="user[firstname]"]')
+        .type('Jane');
+
+      cy.get('input[name="user[lastname]"]')
+        .type('Doe');
+
+      cy.get('input[name="user[email]"]')
+        .type('jane_doe@mail.ca');
+
+      cy.get('input[name="user[password]"]')
+        .type('password');
+
+      cy.get('input[name="user[password_confirmation]"]')
+        .type('password');
+
+      cy.get('input[name="commit"]')
+        .click();
+
+      cy.contains('#navbarSupportedContent', 'Jane');
+
+      cy.contains('#navbarSupportedContent', 'Logout');
+
+    });
+
+  });
+
+  context('login sequence only', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000');
+    });
+
+    it("should login and navigate to homepage", () => {
+      cy.contains('Login')
+        .click();
+
+      cy.get('input[name="email"]')
+        .type('john_doe@mail.ca');
+
+      cy.get('input[name="password"]')
+        .type('password');
+
+      cy.get('input[name="commit"]')
+        .click();
+
+      cy.visit('http://localhost:3000')
+        .get('.button_to')
+        .first()
+        .submit();
+
+      cy.contains('#navbarSupportedContent', 'John');
+
+      cy.contains('#navbarSupportedContent', 'Logout');
+    });
+
+  });
+});
